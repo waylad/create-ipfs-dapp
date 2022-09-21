0cc226857fa6042e469bc1ab741471f60021d4c6
waylad <waylad42@gmail.com>
2022-09-22 00:08:11 +0200

    Add and test ipfs

--- packages/cra-template/template/src/App.js
+++ packages/cra-template/template/src/App.js
@@ -1,14 +1,37 @@
+import { Buffer } from 'buffer/';
+import { create } from 'ipfs-http-client';
+
 import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const upload = async () => {
+    const projectId = '29iQPvAdeIYGWCcM2wOQZLMljbt';
+    const projectSecret = '95b36673339827ad7547cf8aff56d82b';
+    const auth =
+      'Basic ' +
+      Buffer.from(projectId + ':' + projectSecret).toString('base64');
+    const client = create({
+      host: 'ipfs.infura.io',
+      port: 5001,
+      protocol: 'https',
+      headers: {
+        authorization: auth,
+      },
+    });
+    const { cid } = await client.add('Hello world!');
+    console.log('cid', cid);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={() => upload()}>Upload</button>
         <a
           className="App-link"
           href="https://reactjs.org"
